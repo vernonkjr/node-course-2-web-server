a20f375a102ffbe3ffda77ccff413481870074df
vernonkjr <vkerrjr@gmail.com>
2018-02-23 23:40:17 -0500

    Creating a Web Server

--- server.js
+++ server.js
@@ -2,6 +2,8 @@ const express = require('express');
 
 const app = express();
 
+app.use(express.static(__dirname + '/public'));
+
 app.get('/', (req, res) => {
   // res.send('<h1>Hello Express!</h1>');
   res.send({
@@ -24,4 +26,6 @@ app.get('/bad', (req, res) => {
   });
 })
 
-app.listen(3000);
+app.listen(3000, () => {
+  console.log('Server is up on port 3000');
+});
