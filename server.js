7975f13b1af6eaf4fd38c37e5cfec4cebbd6c5ad
vernonkjr <vkerrjr@gmail.com>
2018-02-25 11:32:35 -0500

    Express Middleware

--- server.js
+++ server.js
@@ -1,12 +1,31 @@
 const express = require('express');
 const hbs = require('hbs');
+const fs = require('fs');
 
 const app = express();
 
 hbs.registerPartials(__dirname + '/views/partials');
 app.set('view engine', 'hbs');
 
-app.use(express.static(__dirname + '/public'));
+// this is our custom middleware to log request info and timestamp
+app.use((req, res, next) => {
+  const now = new Date().toString();
+  const log = `${now}: ${req.method} ${req.url}`;
+  console.log(log);
+  fs.appendFile('server.log', log + '\n', (err) => {
+    if (err) {
+      console.log('Unable to append to server.log.');
+    }
+  });
+  next();
+}); // more middleware
+
+// down for repair middleware
+// app.use((req, res, next) => {
+//   res.render('maintenance.hbs');
+// });
+
+app.use(express.static(__dirname + '/public')); // middleware
 
 hbs.registerHelper('getCurrentYear', () => {
   return new Date().getFullYear();
